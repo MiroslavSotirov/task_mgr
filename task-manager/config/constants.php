5549761625595552280c317319e7015d73bd436d
Miroslav Sotirov <miroslavsotirov@gmail.com>
2023-05-10 11:27:10 +0300

    sdfsdf

---
+++ task-manager/config/constants.php
@@ -0,0 +1,13 @@
+<?php 
+//Start Session
+session_start();
+
+//Create Constants to save Database Credentials
+define('LOCALHOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'task_mgr');
+
+define('SITEURL', 'http://localhost/php/task-manager/');
+
+?>
